b75c42690169f15bb986eeaf448c01bd57cddbf7
Renan Porto <renan.porto1099@gmail.com>
2020-04-13 16:50:19 -0300

    Script para analisar dominios

---
+++ run.py
@@ -0,0 +1,26 @@
+import socket
+import pandas as pd
+import xlsxwriter
+
+excelPath = '/PATH/TO/EXCEL'
+
+workbook = xlsxwriter.Workbook(excelPath)
+worksheet = workbook.add_worksheet()
+
+data = pd.read_excel (excelPath)
+rows = pd.DataFrame(data, columns= ['Domain'])
+
+row = 0
+#print(domains)
+for domain in rows['Domain']:
+    result = ''
+    try:
+        row = row + 1
+        result = socket.gethostbyname(domain)
+    except Exception as exc:
+        result = exc
+    finally:
+        worksheet.write("{}{}".format("B", row), str(result))
+
+
+workbook.close()
